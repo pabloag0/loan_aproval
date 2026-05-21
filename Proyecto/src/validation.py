b2e745ae78e0de5f790da56ab045aa570b9a4ef2
pabloag0 <pablag02@ucm.es>
2026-05-21 10:30:00 +0200

    Validacion

--- Proyecto/src/validation.py
+++ Proyecto/src/validation.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split 
 
 ##############################################################################
 # CROSS VALIDATION
 
-def cross_validate(X, y, train: function, predict: function):
+def cross_validate(X, y, train: function, predict: function, folds=5):
 
+    for i in range(folds):
+        X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
+    
 
 
 
